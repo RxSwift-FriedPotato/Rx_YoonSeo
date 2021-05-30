cd9c75f6cbf102d86a7e2c84756f124fbec3463c
Yoonseo Kim <72497599+ezidayzi@users.noreply.github.com>
2021-05-30 15:22:29 +0900

    [DELETE] MVVM-Practice 삭제

--- MVVM-Practice/MVVM-Practice/Model/Model.swift
+++
@@ -1,8 +0,0 @@
-//
-//  Model.swift
-//  MVVM-Practice
-//
-//  Created by 김윤서 on 2021/05/30.
-//
-
-import Foundation
--- MVVM-Practice/Pods/Target Support Files/Pods-MVVM-Practice/Pods-MVVM-Practice-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_MVVM_Practice : NSObject
-@end
-@implementation PodsDummy_Pods_MVVM_Practice
-@end
